65acc45c1053a7ffd9a7a899bd0de3876a11f54e
Shouider <supershouider@gmail.com>
2025-05-10 17:46:48 +0200

    balanced seesaw-vase interaction

--- Assets/Scripts/Seesaw.cs
+++ Assets/Scripts/Seesaw.cs
@@ -16,7 +16,7 @@ public class Seesaw : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
 
         float horizontal = Input.GetAxis("Horizontal");
