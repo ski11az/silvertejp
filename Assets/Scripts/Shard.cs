bd62d4e8627865325dc6f4873d466e0d28722758
Shouider <supershouider@gmail.com>
2025-05-10 20:33:03 +0200

    Added max speed

--- Assets/Scripts/Shard.cs
+++ Assets/Scripts/Shard.cs
@@ -5,17 +5,30 @@ public class Shard : MonoBehaviour
     public bool isAttached = false;
     Vase parentVase = null;
     Rigidbody2D rb;
+    static float maxSpeed = 3f;
 
     private void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
     }
 
+    void FixedUpdate()
+    {
+        // Limits 
+        if(rb != null && rb.velocity.magnitude > maxSpeed)    
+        {
+            rb.velocity = Vector3.ClampMagnitude(rb.velocity, maxSpeed);
+        }
+    }
+
+
     public void Attach(Vase vase)
     {
         Destroy(rb);
         isAttached = true;
         parentVase = vase;
+        //maxSpeed = maxSpeed + 0.5;
+        Debug.Log(maxSpeed);
     }
 
     public void Detach()
